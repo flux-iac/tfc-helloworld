54831469b69a162a4d1979aacab4d7f1b62a7882
Chanwit Kaewkasi <chanwit@gmail.com>
2022-01-15 18:50:24 +0700

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -9,6 +9,13 @@ terraform {
   }
 }
 
+variable "subject" {
+   type = string
+   default = "World TFC"
+   description = "Subject to hello"
+}
+
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
-  value = "Hello, World TFC!"
+  value = "Hello, ${var.subject}!"
 }
